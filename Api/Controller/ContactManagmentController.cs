15e7ce07b4c84d2ddb4840234b5806e99e94926d
Artsiom_Markushevski <lakopatidan@gmail.com>
2025-11-18 21:35:06 +0300

    Создал метод добавление контактов

---
+++ Api/Controller/ContactManagmentController.cs
@@ -0,0 +1,24 @@
+﻿using Api.Storage;  // ← добавить эту строку
+using Api.Contact;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controller
+{
+    public class ContactManagmentController : Basecontroller
+    {
+        private readonly ContactStorage storage;
+        public ContactManagmentController(ContactStorage storage)
+        {
+            this.storage = storage;
+        }
+        [HttpPost("contacts")]
+        public void Create(Api.Contact.Contact contact)
+        {
+            
+        }
+    }
+
+
+    
+}
