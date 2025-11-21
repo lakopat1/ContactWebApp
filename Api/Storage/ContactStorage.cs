e657c662a5744ba41a526847cecd8863629bd745
Artsiom_Markushevski <lakopatidan@gmail.com>
2025-11-21 14:29:37 +0300

    feat(storage): генерация фейковых данных

--- Api/Storage/ContactStorage.cs
+++ Api/Storage/ContactStorage.cs
@@ -7,6 +7,16 @@
         public ContactStorage()
         {
             this.Contacts = new List<Api.Contact.Contact>();
+            for(int i = 1; i < 5; i++)
+            {
+                this.Contacts.Add (new Api.Contact.Contact()
+                {
+                    Id = i,
+                    Name = $"Полное имя {i}",
+                    Email = $"{Guid.NewGuid().ToString().Substring(0,5)}_{i}@mail.ru"
+
+                });
+            }
         }
 
     }
